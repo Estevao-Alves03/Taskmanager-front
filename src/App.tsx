0aa0f43290a43d416567d9345ee63e6f199dc7f7
Estevao-Alves03 <estevao.galves24@gmail.com>
2025-06-08 22:01:12 -0300

    starting new project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Home from "./Components/Pages/Home"
+
+function App() {
+  
+  return (
+      Home
+  )
+}
+
+export default App
---
+++ src/Components/Pages/Home.jsx
@@ -0,0 +1,7 @@
+function Home(){
+    return(
+        <h1>Teste da pagina principal</h1>
+    );
+}
+
+export default Home()
